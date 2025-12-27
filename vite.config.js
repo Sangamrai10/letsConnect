3063418323f1d039f42606293bc44803aca2ea5d
sangam <ghub487@gmail.com>
2025-12-27 19:37:21 +0545

    starting point

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react'; // Include other framework plugins if used
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    tailwindcss(),
+    react(),
+  ],
+});
